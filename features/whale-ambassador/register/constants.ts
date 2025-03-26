f0066088d93858ba8763644cc57ce6b5918d586d
石石 <lei.yang@longbridge-inc.com>
2025-03-26 09:42:23 +0800

    fix: 修复其它选项问题

--- features/whale-ambassador/register/constants.ts
+++ features/whale-ambassador/register/constants.ts
@@ -4,11 +4,11 @@ import { useTranslation } from 'react-i18next'
 export const useServiceOptions = () => {
   const { t } = useTranslation('common')
   return [
-    { label: t('whale-ambassador.internet-app-plus'), value: Service.INTERNET_APP_PLUS.toString() },
-    { label: t('whale-ambassador.desk-system'), value: Service.DESK_SYSTEM.toString() },
-    { label: t('whale-ambassador.us-stock-and-options'), value: Service.US_STOCK_AND_OPTIONS.toString() },
-    { label: t('whale-ambassador.wealth-management'), value: Service.WEALTH_MANAGEMENT.toString() },
-    { label: t('whale-ambassador.virtual-asset'), value: Service.VIRTUAL_ASSET.toString() },
-    { label: t('whale-ambassador.other'), value: Service.OTHER.toString() },
+    { label: t('whale-ambassador.internet-app-plus'), value: Service.INTERNET_APP_PLUS },
+    { label: t('whale-ambassador.desk-system'), value: Service.DESK_SYSTEM },
+    { label: t('whale-ambassador.us-stock-and-options'), value: Service.US_STOCK_AND_OPTIONS },
+    { label: t('whale-ambassador.wealth-management'), value: Service.WEALTH_MANAGEMENT },
+    { label: t('whale-ambassador.virtual-asset'), value: Service.VIRTUAL_ASSET },
+    { label: t('whale-ambassador.other'), value: Service.OTHER },
   ]
 }
